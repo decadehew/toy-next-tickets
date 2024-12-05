6f489db119ec458490218ee9bd456c923b5cc9d4
Decade Hew <decadehew@DecadedeMacBook-Pro.local>
2024-12-05 21:44:16 +0800

    使用 useTransition 在表單提交時顯示加載狀態，優化用戶體驗

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useTransition } from 'react'
 import { Ticket } from '@prisma/client'
 
 import { Button } from '@/components/ui/button'
@@ -5,16 +8,20 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
 import { upsertTicket } from '../actions/upsert-ticket'
+import { LucideLoaderCircle } from 'lucide-react'
 
 interface TicketUpsertFormProps {
   ticket?: Ticket
 }
 export const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
+  const [isPending, startTransition] = useTransition()
+  const upsertTicketAction = async (formData: FormData) => {
+    startTransition(async () => {
+      await upsertTicket(ticket?.id, formData)
+    })
+  }
   return (
-    <form
-      action={upsertTicket.bind(null, ticket?.id)}
-      className="flex flex-col gap-y-2"
-    >
+    <form action={upsertTicketAction} className="flex flex-col gap-y-2">
       <Label htmlFor="title">Title</Label>
       <Input id="title" type="text" name="title" defaultValue={ticket?.title} />
 
@@ -26,7 +33,10 @@ export const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
         defaultValue={ticket?.content}
       />
 
-      <Button type="submit">{ticket ? 'Edit' : 'Create'}</Button>
+      <Button type="submit" disabled={isPending}>
+        {isPending && <LucideLoaderCircle className="h-4 w-4 animate-spin" />}
+        {ticket ? 'Edit' : 'Create'}
+      </Button>
     </form>
   )
 }
