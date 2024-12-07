625b8f6715c60bb69477f4e93babda0b0e8a2d78
Decade Hew <decadehew@DecadedeMacBook-Pro.local>
2024-12-07 18:42:54 +0800

    form validate & reset

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -1,26 +1,37 @@
 'use server'
 
+import { z } from 'zod'
 import { prisma } from '@/lib/prisma'
 import { ticketPath, ticketsPath } from '@/paths'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
+const UpsertTicketSchema = z.object({
+  title: z.string().min(1).max(191),
+  content: z.string().min(1).max(1024),
+})
+
 const upsertTicket = async (
   id: string | undefined,
   // 請查看筆記
-  _actionState: { message: string },
+  _actionState: { message: string; payload?: FormData },
   formData: FormData
 ) => {
-  const data = {
-    title: formData.get('title') as string,
-    content: formData.get('content') as string,
-  }
+  try {
+    const data = UpsertTicketSchema.parse({
+      title: formData.get('title'),
+      content: formData.get('content'),
+    })
 
-  await prisma.ticket.upsert({
-    where: { id: id || '' },
-    create: data,
-    update: data,
-  })
+    await prisma.ticket.upsert({
+      where: { id: id || '' },
+      create: data,
+      update: data,
+    })
+  } catch (error) {
+    console.error(error)
+    return { message: 'Something went wrong!', payload: formData }
+  }
 
   revalidatePath(ticketsPath())
 
--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -22,14 +22,23 @@ export const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
   return (
     <form action={formAction} className="flex flex-col gap-y-2">
       <Label htmlFor="title">Title</Label>
-      <Input id="title" type="text" name="title" defaultValue={ticket?.title} />
+      <Input
+        id="title"
+        type="text"
+        name="title"
+        defaultValue={
+          (actionState.payload?.get('title') as string) ?? ticket?.title
+        }
+      />
 
       <Label htmlFor="content">Content</Label>
       <Input
         id="content"
         type="text"
         name="content"
-        defaultValue={ticket?.content}
+        defaultValue={
+          (actionState.payload?.get('content') as string) ?? ticket?.content
+        }
       />
 
       <SubmitButton label={ticket ? 'Edit' : 'Create'} />
