f87d49ca157637d208ac05e4eeec4b54106ea366
decadehew <yong_323@hotmail.com>
2025-01-13 23:56:23 +0800

    updated

---
+++ src/features/auth/queries/get-auth-or-redirect.ts
@@ -0,0 +1,11 @@
+import { redirect } from 'next/navigation'
+import { signInPath } from '@/paths'
+import { getAuth } from '@/features/auth/queries/get-auth'
+
+export const getAuthOrRedirect = async () => {
+  const { user } = await getAuth()
+
+  if (!user) {
+    redirect(signInPath())
+  }
+}
